ed3c7f85f589e83bcab6dff65381e574fe9af436
Burger <9020693@student.zadkine.nl>
2022-11-30 12:09:43 +0100

    Update + uitleg game placeholder content added

--- app.js
+++ app.js
@@ -8,3 +8,11 @@
 // document.addEventListener('DOMContentLoaded', function(event) {
 //     document.querySelector('body').style.opacity = 1
 // })
+
+// Updates the age of Roberto automaticly
+let now = new Date();
+let birth = new Date('2005-08-23');
+let difference = now - birth;
+let age = new Date(difference).getFullYear() - 1970
+let span = document.getElementById('age');
+span.innerText = age;
