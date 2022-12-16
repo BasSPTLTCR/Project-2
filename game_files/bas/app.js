42dda97b1a14c842b82169b2c971238b66d501df
BasVanDenBlink <9014541@student.zadkine.nl>
2022-12-16 22:59:34 +0100

    grote progres met de movement en muren toegevoegd

--- game_files/bas/app.js
+++ game_files/bas/app.js
@@ -6,6 +6,10 @@ let playerPosition = null
 let start = null
 let start2 = null
 let start3 = null
+let nextMoveD = null
+let nextMoveA = null
+let nextMoveW = null
+let nextMoveS = null
 
 function createGrid(rows, cols) {
   // Get a reference to the container element
@@ -44,6 +48,15 @@ function createGrid(rows, cols) {
 
         }
         
+        // Maze randomization
+        if (cols == 10 && (j > 0 && j < 9) && i < 9) {
+          x = Math.floor(Math.random() * 10)
+          console.log(x);
+          if (x >= 6) {
+            cell.setAttribute("class", "wall")
+          }
+        }
+
         // Add the cell to the row
         row.appendChild(cell);
         
@@ -54,47 +67,69 @@ function createGrid(rows, cols) {
     
     // Add the table to the container
     container.appendChild(table);
-    
-    if (cols == 10) {
-      // .setAttribute("class", "playerHead")
-    }
 }
 
 function load() {
   start = document.getElementById(105).setAttribute("class", "playerHead")
+  // start2 = 
+  // start3 =
   playerPosition = +document.getElementsByClassName("playerHead")[0].id
   wall = document.getElementsByClassName("wall")
   
 
 
   window.addEventListener("keydown", function(event){
-    // Looks for when the key d is pressed
-    if (event.key == "d") {
+    // Looks for when the key d is pressed and if the movement that is about to perform will hit a div with the class wall and as long as the next movement that is about to run is going into the finish
+    if (event.key == "d" && this.document.getElementById(nextMoveD).classList.contains("wall") != true && this.document.getElementById(nextMoveD).classList.contains("exit") != true) {
+
+      // These three lines will provide moving the player to the right
       this.document.getElementById(playerPosition).classList.remove("playerHead")
       playerPosition = playerPosition + 100 
       this.document.getElementById(playerPosition).classList.add("playerHead")
+
+      // These four lines will update the next movement of the player on every step
       nextMoveD = playerPosition + 100
+      nextMoveA = playerPosition - 100
+      nextMoveW = playerPosition - 1
+      nextMoveS = playerPosition + 1
+
+      // Here it will look for the d key to be pressed and when the exit is in the providing move
+    } else if (event.key == "d" && this.document.getElementById(nextMoveD).classList.contains("exit") == true){
+      this.document.getElementById(playerPosition).classList.remove("playerHead")
+      playerPosition = playerPosition + 100 
+      this.document.getElementById(playerPosition).classList.remove("exit")
+      this.document.getElementById(playerPosition).classList.add("playerHead")
+      // Display finish
     }
     // Looks for when the key a is pressed
-    if (event.key == "a") {
+    if (event.key == "a" && (nextMoveA != 5 && this.document.getElementById(nextMoveA).classList.contains("wall") != true)) {
       this.document.getElementById(playerPosition).classList.remove("playerHead")
       playerPosition = playerPosition - 100
       this.document.getElementById(playerPosition).classList.add("playerHead")
-      nextMoveA = playerPosition + 100
+      nextMoveD = playerPosition + 100
+      nextMoveA = playerPosition - 100
+      nextMoveW = playerPosition - 1
+      nextMoveS = playerPosition + 1
     }
     // Looks for when the key w is pressed
-    if (event.key == "w") {
+    if (event.key == "w" && (nextMoveW > 105 && this.document.getElementById(nextMoveW).classList.contains("wall") != true)) {
       this.document.getElementById(playerPosition).classList.remove("playerHead")
       playerPosition = playerPosition - 1
       this.document.getElementById(playerPosition).classList.add("playerHead")
-      nextMoveW = playerPosition + 100
+      nextMoveD = playerPosition + 100
+      nextMoveA = playerPosition - 100
+      nextMoveW = playerPosition - 1
+      nextMoveS = playerPosition + 1
     }
     // Looks for whhen the key s is pressed
-    if (event.key == "s") {
+    if (event.key == "s" && this.document.getElementById(nextMoveS).classList.contains("wall") != true) {
       this.document.getElementById(playerPosition).classList.remove("playerHead")
       playerPosition = playerPosition + 1
       this.document.getElementById(playerPosition).classList.add("playerHead")
-      nextMoveS = playerPosition + 100
+      nextMoveD = playerPosition + 100
+      nextMoveA = playerPosition - 100
+      nextMoveW = playerPosition - 1
+      nextMoveS = playerPosition + 1
     }
     console.log(playerPosition);
     })
@@ -105,10 +140,13 @@ function load() {
 
 function easyMode() {
   createGrid(10, 10)
-  const outerWalls = 
   btn.style.display = "none"
   btn2.style.display = "none"
   btn3.style.display = "none"
+  nextMoveD = 105 + 100
+  nextMoveA = 105 - 100
+  nextMoveW = 105 - 1
+  nextMoveS = 105 + 1
   setTimeout(load, 0)
   
 }
