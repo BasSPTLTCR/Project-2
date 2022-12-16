1f94d93f0f812d5a1c23aad734b156494cbf8371
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-12-16 12:18:13 +0100

    javascript changes + uitleg games

--- game_files/bastiaan/scripts/game.js
+++ game_files/bastiaan/scripts/game.js
@@ -2,10 +2,22 @@ let start = document.getElementById("startGame")
 let menuMusic = new Audio("../game_files/bastiaan/sounds/DasGrosseGrillen.mp3")
 let gameStarter = document.getElementById("gamestarter")
 let settingsButton = document.getElementById("settings")
+let creditsButton = document.getElementById("credits")
 
+// function that gets the menu set up
 function loadMenu() {
 menuMusic.play()
+//hides the start button and shows the menu buttons
 start.style.display = "none"
 gameStarter.style.display = "block"
 settingsButton.style.display = "block"
+creditsButton.style.display = "block"
+}
+
+function loadGame() {
+    console.log("loading game!")
+}
+
+function loadSettings() {
+    console.log("settings loading!")
 }
