7855fd8b2360395dfa342a542af23e7133047007
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-12-16 12:05:02 +0100

    css game menu fixed

--- game_files/bastiaan/scripts/game.js
+++ game_files/bastiaan/scripts/game.js
@@ -1,9 +1,11 @@
 let start = document.getElementById("startGame")
 let menuMusic = new Audio("../game_files/bastiaan/sounds/DasGrosseGrillen.mp3")
 let gameStarter = document.getElementById("gamestarter")
+let settingsButton = document.getElementById("settings")
 
 function loadMenu() {
 menuMusic.play()
 start.style.display = "none"
 gameStarter.style.display = "block"
+settingsButton.style.display = "block"
 }
