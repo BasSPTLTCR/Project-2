2dc0608b674b3dc7d221b50df9b1057aea75c21f
BastiaanvanDelft <9019711@student.zadkine.nl>
2022-12-19 14:57:25 +0100

    menu done

--- game_files/bastiaan/scripts/game.js
+++ game_files/bastiaan/scripts/game.js
@@ -1,23 +1,68 @@
+//buttons
 let start = document.getElementById("startGame")
-let menuMusic = new Audio("../game_files/bastiaan/sounds/DasGrosseGrillen.mp3")
 let gameStarter = document.getElementById("gamestarter")
 let settingsButton = document.getElementById("settings")
 let creditsButton = document.getElementById("credits")
+let muteButton = document.getElementById("mute")
+let backButton = document.getElementById("back")
+
+//divs and text
+let creditsBlock = document.getElementById("creditsdiv")
+
+//sfx
+let menuMusic = new Audio("../game_files/bastiaan/sounds/DasGrosseGrillen.mp3")
 
 // function that gets the menu set up
 function loadMenu() {
-menuMusic.play()
-//hides the start button and shows the menu buttons
-start.style.display = "none"
-gameStarter.style.display = "block"
-settingsButton.style.display = "block"
-creditsButton.style.display = "block"
+    //starts music
+    menuMusic.play()
+    //loops music
+    menuMusic.loop = true;
+    //hides the start button and shows the menu buttons
+    start.style.display = "none"
+    gameStarter.style.display = "block"
+    settingsButton.style.display = "block"
+    creditsButton.style.display = "block"
+    if (muteButton) {
+        muteButton.style.display = "none"
+    }
+    if (backButton) {
+        backButton.style.display = "none"
+    }
+    if (creditsBlock) {
+        creditsBlock.style.display = "none"
+    }
 }
 
 function loadGame() {
-    console.log("loading game!")
+    gameStarter.style.display = "none"
+    settingsButton.style.display = "none"
+    creditsButton.style.display = "none"
+
 }
 
 function loadSettings() {
-    console.log("settings loading!")
+    //console.log("settings loading!")
+    gameStarter.style.display = "none"
+settingsButton.style.display = "none"
+creditsButton.style.display = "none"
+muteButton.style.display = "block"
+backButton.style.display = "block"
+}
+
+function musicMute () {
+    if (menuMusic.muted == false) {
+        menuMusic.muted = true
+    }
+    else {
+        menuMusic.muted = false
+    }
+}
+
+function loadCredits() {
+    gameStarter.style.display = "none"
+    settingsButton.style.display = "none"
+    creditsButton.style.display = "none"
+    creditsBlock.style.display = "block"
+    backButton.style.display = "block"
 }
