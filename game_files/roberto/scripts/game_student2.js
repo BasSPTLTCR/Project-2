3f1ee41b1bd93573971dc3fcfef5a899dcd33016
Burger <9020693@student.zadkine.nl>
2023-01-03 14:08:45 +0100

    Updated game

--- game_files/roberto/scripts/game_student2.js
+++ game_files/roberto/scripts/game_student2.js
@@ -55,13 +55,17 @@ const cards = [
 let start_btn = document.getElementById("start");
 let hit_btn = document.getElementById("hit_btn");
 let pass_btn = document.getElementById("pass_btn");
+let reset_btn = document.getElementById("reset_btn");
 /* Cards (Images) */
 let your_first_card = document.getElementById("your_first_card");
 let your_second_card = document.getElementById("your_second_card");
 let your_hit_card_1 = document.getElementById("your_hit_card_1");
 let your_hit_card_2 = document.getElementById("your_hit_card_2");
-let dealer_cards = document.getElementById("dealer_cards");
-let dealer_second_cards = document.getElementById("dealer_second_cards");
+let dealer_card = document.getElementById("dealer_card");
+let dealer_second_card = document.getElementById("dealer_second_card");
+let dealer_third_card = document.getElementById("dealer_third_card");
+let dealer_fourth_card = document.getElementById("dealer_fourth_card");
+let dealer_fifth_card = document.getElementById("dealer_fifth_card");
 /* Shown total values */
 let dealer_value = document.getElementById("dealer_value");
 let player_value = document.getElementById("player_value");
@@ -69,8 +73,6 @@ let player_value = document.getElementById("player_value");
 
 /* Imports audio from the sounds folder. */
 let dealing_fx = new Audio('../game_files/roberto/sounds/dealing.mp3');
-let hit_fx = new Audio('../game_files/roberto/sounds/hit.mp3');
-
 
 /* Default values */
 let src;
@@ -78,49 +80,123 @@ let total_player = 0;
 let total_dealer = 0;
 
 function start() {
+    hit_btn.setAttribute("disabled","disabled")
+    hit_btn.style.backgroundColor = "grey";
+    pass_btn.setAttribute("disabled","disabled")
+    pass_btn.style.backgroundColor = "grey";
+    
+    reset_btn.style.display = "block";
     dealer_value.style.display = "flex";
     player_value.style.display = "flex";
     start_btn.style.display = "none";
-    dealing_fx.play();
-    
-    randomize();
-    your_first_card.src = src;
-    randomize();
-    your_second_card.src = src;
-    randomize_dealer();
-    dealer_cards.src = src_dealer;
-    randomize_dealer();
-    dealer_second_cards.src = src_dealer;
-    dealer_cards.style.display = "block";
-    dealer_second_cards.style.display = "block";
-    your_first_card.style.display = "block";
-    your_second_card.style.display = "block";
+
+    setTimeout(() => {
+        randomize();
+        dealing_fx.play();
+        your_first_card.style.display = "block";
+        your_first_card.src = src;
+    },750);
+    setTimeout(() => {
+        randomize();
+        dealing_fx.play();
+        your_second_card.style.display = "block";
+        your_second_card.src = src;
+    },1500);
+    setTimeout(() => {
+        randomize_dealer();
+        dealing_fx.play();
+        dealer_card.style.display = "block";
+        dealer_card.src = src_dealer;
+    },2250);
+    setTimeout(() => {
+        dealing_fx.play();
+        dealer_second_card.style.display = "block";
+        dealer_second_card.src = "http://127.0.0.1:5500/game_files/roberto/textures/cards/card_back.png";
+    },3000);
     hit_btn.style.display = "block";
     pass_btn.style.display = "block";
-    console.log("You have: " + total_player)
-    console.log("Dealer has: " + total_dealer);
+    setTimeout(() => {
+        hit_btn.removeAttribute("disabled");
+        pass_btn.removeAttribute("disabled");
+        hit_btn.style.backgroundColor = "rgb(24, 209, 24)";
+        pass_btn.style.backgroundColor = "rgb(255, 0, 0)";
+    },3000);
 }
 
 function hit() {
     let count = 0;
     count += 1;
-    hit_fx.play();
     if (count == 1) {
-        your_hit_card_1.style.display = "block";
-        your_hit_card_1.src = src;
+        setTimeout(() => {
+            randomize();
+            dealing_fx.play();
+            your_hit_card_1.style.display = "block";
+            your_hit_card_1.src = src;
+        },750);
     }
     if (count == 2) {
-        your_hit_card_2.style.display = "block";
-        your_hit_card_2.src = src;
+        setTimeout(() => {
+            randomize();
+            dealing_fx.play();
+            your_hit_card_2.style.display = "block";
+            your_hit_card_2.src = src;
+        },750);
     }
-    randomize();
-    // randomize();
-    // dealer_second_cards.src = src;
-    // dealer_second_cards.style.display = "block";
+    check();
+    dealing_fx.play();
+    randomize_dealer();
+    dealer_second_card.src = src_dealer;
+    hit_btn.style.backgroundColor = "grey";
+    hit_btn.setAttribute("disabled","disabled")
 }
 
-function pass(){
+function pass() {
+    check();
+    dealing_fx.play();
+    randomize_dealer();
+    dealer_second_card.src = src_dealer;
+    pass_btn.setAttribute("disabled","disabled")
+    pass_btn.style.backgroundColor = "grey";
+}
 
+function check(){
+    for (let i = 0; total_dealer <= 16; i++) {
+            let count_dealer = 0;
+            count_dealer += 1;console.log("Dealer var: " + total_dealer)
+            if(count_dealer == 1){
+                randomize_dealer();
+                dealing_fx.play();
+                dealer_third_card.style.display = "block";
+                dealer_third_card.src = src_dealer;
+            }
+            if(count_dealer == 2){
+                randomize_dealer();
+                dealing_fx.play();
+                dealer_fourth_card.style.display = "block";
+                dealer_fourth_card.src = src_dealer;
+            }
+            if(count_dealer == 3){
+                randomize_dealer();
+                dealing_fx.play();
+                dealer_fifth_card.style.display = "block";
+                dealer_fifth_card.src = src_dealer;
+            }
+        }
+    if (total_player > total_dealer) {
+        console.log("You won");
+    }
+    if (total_player < total_dealer) {
+        console.log("You lost");
+    }
+    if (total_player == total_dealer) {
+        console.log("Draw");
+    }
+    if (total_player == 21) {
+        console.log("you won")
+    }
+    if (total_dealer > 21) {
+        console.log("you lost")
+    }
 }
 
 function randomize() {
@@ -143,68 +219,6 @@ function randomize_dealer() {
     dealer_value.innerText = "Dealer: " + total_dealer;
 }
 
-// // Define the ranks and suits of the cards
-// const ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'];
-// const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-
-// // Create a deck of cards as an array of objects
-// let deck = [];
-// for (let rank of ranks) {
-//   for (let suit of suits) {
-//     let value = parseInt(rank);
-//     if (isNaN(value)) {
-//       if (rank === 'Ace') {
-//         value = 11;
-//       } else {
-//         value = 10;
-//       }
-//     }
-//     deck.push({ rank: rank, suit: suit, value: value });
-//   }
-// }
-
-// // Shuffle the deck using the Fisher-Yates shuffle algorithm
-// function shuffle(array) {
-//   for (let i = array.length - 1; i > 0; i--) {
-//     let j = Math.floor(Math.random() * (i + 1));
-//     [array[i], array[j]] = [array[j], array[i]];
-//   }
-// }
-// shuffle(deck);
-
-// // Deal two cards to the player and two cards to the dealer
-// let playerHand = [deck.shift(), deck.shift()];
-// let dealerHand = [deck.shift(), deck.shift()];
-
-// // Calculate the total value of a hand
-// function getTotalValue(hand) {
-//   let totalValue = 0;
-//   let numAces = 0;
-//   for (let i = 0; i < hand.length; i++) {
-//     let card = hand[i];
-//     totalValue += card.value;
-//     if (card.rank === 'Ace') {
-//       numAces++;
-//     }
-//   }
-//   // If the hand contains an Ace, check if changing its value to 1 would
-//   // result in a better total value
-//   while (numAces > 0 && totalValue > 21) {
-//     totalValue -= 10;
-//     numAces--;
-//   }
-//   return totalValue;
-// }
-
-// // Calculate the total values of the player's and dealer's hands
-// let playerTotal = getTotalValue(playerHand);
-// let dealerTotal = getTotalValue(dealerHand);
-
-// // Determine the winner of the game
-// if (playerTotal > 21) {
-//   console.log('You lose!');
-// } else if (dealerTotal > 21 || dealerTotal < playerTotal) {
-//   console.log('You win!');
-// } else {
-//   console.log('You lose!');
-// }
+function reload() {
+    location.reload();
+}
